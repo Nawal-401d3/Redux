059f805e0cfcf037e963b3eaebdef0baba9e97df
Nsuliman <nsuliman88@gmail.com>
2020-07-12 17:11:00 +0300

    All needed files are added

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+// to be able to provide our store to app 
+import { Provider } from 'react-redux';
+import App from './app.js';
+
+import './app.scss';
+
+// import store from './store/index.js';
+
+function Entry() {
+  return (
+    // <Provider store={store}>
+    //   <App />
+    // </Provider>
+    <>
+    </>
+  );
+}
+
+const rootPoint = document.getElementById('root');
+ReactDom.render(<Entry />, rootPoint);
